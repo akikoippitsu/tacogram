4f3afb2d5abd91b91894c2a311d165e3fbb5abe0
akikoippitsu <akiko.ippitsu@kellogg.northwestern.edu>
2026-05-28 18:48:48 -0500

    Add user authentication logic to create action
    Implement user authentication in create action.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,27 @@
 class SessionsController < ApplicationController
   def new
   end
-  
+
   def create
-    # TODO: authenticate user
-    flash["notice"] = "Nope."
+    @user = User.find_by({ "email" => params["email"] })
+    if @user == nil
+      flash["notice"] = "No user with that email found."
+      redirect_to "/login"
+    else
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome back, #{@user["first_name"]}!"
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Incorrect password."
+        redirect_to "/login"
+      end
+    end
+  end
+
+  def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Logged out."
     redirect_to "/login"
   end
 end
